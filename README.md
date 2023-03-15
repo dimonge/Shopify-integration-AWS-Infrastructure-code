7f51aeb6823d019e69628e7fcdf9f878b3ccd071
Peter Shodeinde <petershodeinde4@gmail.com>
2023-03-15 17:30:43 +0200

    Shopify integration AWS Infrastructure code

---
+++ README.md
@@ -0,0 +1,1 @@
+# Shopify integration AWS Infrastructure code
---
+++ test/genesis-db.test.js
@@ -0,0 +1,18 @@
+// const { Template } = require('@aws-cdk/assertions');
+// const cdk = require('@aws-cdk/core');
+// const GenesisDb = require('../lib/genesis-db-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/genesis-db-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new GenesisDb.GenesisDbStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
+
